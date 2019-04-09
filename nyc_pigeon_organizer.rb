43f286eda2b3e79cb0225d6349d865e50307ecc5
John Hill <john.m.hill91@gmail.com>
2019-04-09 23:13:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pidgeonByName={}
+  data.each do |data, hash|
+    hash.each do |details, names|
+      names.each do |name|
+       pidgeonByName[name] = {:color => [], :gender => [], :lives => []} 
+      end
+    end
+  end
+  data[:color].each do |shade, arr|
+    arr.each do |name|
+      if data[:color][shade].include?(name)
+        pidgeonByName[name][:color] << shade.to_s
+      end
+    end
+  end
+
+  data[:gender].each do |sex, arr|
+    arr.each do |name|
+      if data[:gender][sex].include?(name)
+        pidgeonByName[name][:gender] << sex.to_s
+      end
+    end
+  end
+
+  data[:lives].each do |location, arr|
+    arr.each do |name|
+      if data[:lives][location].include?(name)
+        pidgeonByName[name][:lives] << location
+      end
+    end
+  end
+   pidgeonByName 
 end
