4f37715109a8acbd37da9901b9383fca83571781
Joseph G Syverson <josephgavinsyverson@gmail.com>
2019-04-16 01:12:42 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hsh = {}
+  data.each do |div, subdiv|
+    subdiv.each do |specific, names|
+      names.each do |name|
+        new_hsh[name] = {color: [], gender: [], lives: []}
+      end
+    end
+  end
+  new_hsh.each do |name, hash|
+    hash.each do |category_1, specifics|
+      data.each do |div, subdiv|
+        subdiv.each do |specific, names_arr|
+          if names_arr.include?(name)
+            new_hsh[name][div] << specific.to_s
+            new_hsh[name][div].uniq!
+          end
+        end
+      end
+    end
+  end
+  new_hsh
 end
